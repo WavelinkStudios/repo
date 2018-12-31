c40692801a93faa47a4b5e138958f9ca75813324
TD Theos <tdtheos@TDs-MacBook-Pro.hsd1.pa.comcast.net>
2018-12-30 19:19:43 -0500

    BIDsXI Once Again
    Once again trying to get BIDsXI on the repo but it appears that it now works.

--- update-repo.sh
+++ update-repo.sh
@@ -6,3 +6,6 @@ echo 'bzipping and gzipping Packages file'
 bzip2 -c9 ./Packages > ./Packages.bz2
 gzip -c9 ./Packages > ./Packages.gz
 echo 'Done. You may now upload your stuff to the internet.'
+
+# sudo dpkg-scanpackages ./debs /dev/null > Packages
+# The above command is how I have to do for scanning packages after cd'ing into the proper directory
